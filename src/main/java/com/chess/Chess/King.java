7754366dbd43aba5e53d4fdea18baa677b70a53b
Valeria <leralitvinchukk@gmail.com>
2023-11-21 17:11:14 +0200

    changes for castling in king class

--- src/main/java/com/chess/Chess/King.java
+++ src/main/java/com/chess/Chess/King.java
@@ -54,26 +54,26 @@ public class King extends Figure {
   public boolean can_castle(Figure[][] curr_position, int new_row, int new_column) {
     int row = this.return_coordinates()[0];
 
-    if (this.not_moved() == false) {
+    if (!this.not_moved()) {
       return false;
     }
 
     // if king is in check return false
 
     if (new_row == row) {
-      if (new_column == 6) { // short castling
-        if (curr_position[row][7] != null && curr_position[row][7].not_moved() == true) {
-          if (curr_position[row][5] == null && curr_position[row][6] == null) {
-            // check if positions (row, 5) (row, 6) puts king in check
+      if (new_column == 1) { // short castling
+        if (curr_position[row][0] != null && curr_position[row][0].not_moved()) {
+          if (curr_position[row][2] == null && curr_position[row][1] == null) {
+            // check if positions (row, 2) (row, 1) puts king in check
             return true;
           }
 
         }
 
-      } else if (new_column == 2) { // long castling
-        if (curr_position[row][0] != null && curr_position[row][0].not_moved() == true) {
-          if (curr_position[row][3] == null && curr_position[row][2] == null && curr_position[row][1] == null) {
-            // check if positions (row, 3) (row, 2) (row, 1) puts king in check
+      } else if (new_column == 5) { // long castling
+        if (curr_position[row][7] != null && curr_position[row][7].not_moved()) {
+          if (curr_position[row][4] == null && curr_position[row][5] == null && curr_position[row][6] == null) {
+            // check if positions (row, 4) (row, 5) (row, 6) puts king in check
             return true;
           }
         }
@@ -88,30 +88,30 @@ public class King extends Figure {
     if (this.check_if_move_possible(curr_position, new_row, new_column)) {
 
       if (can_castle(curr_position, new_row, new_column)) {
-        if (new_column > 4) { // short castling
+        if (new_column == 1) { // short castling
           // moving king
-          curr_position[new_row][6] = this;
-          curr_position[new_row][4] = null;
-          curr_position[new_row][6].set_coordinates(new_row, 6);
-          curr_position[new_row][6].set_not_moved(false);
+          curr_position[new_row][1] = this;
+          curr_position[new_row][3] = null;
+          curr_position[new_row][1].set_coordinates(new_row, 1);
+          curr_position[new_row][1].set_not_moved(false);
 
           // moving rook
-          curr_position[new_row][5] = curr_position[new_row][7];
-          curr_position[new_row][7] = null;
-          curr_position[new_row][5].set_coordinates(new_row, 5);
-          curr_position[new_row][5].set_not_moved(false);
-        } else { // long castling
-          // moving king
-          curr_position[new_row][2] = this;
-          curr_position[new_row][4] = null;
+          curr_position[new_row][2] = curr_position[new_row][0];
+          curr_position[new_row][0] = null;
           curr_position[new_row][2].set_coordinates(new_row, 2);
           curr_position[new_row][2].set_not_moved(false);
+        } else { // long castling
+          // moving king
+          curr_position[new_row][5] = this;
+          curr_position[new_row][3] = null;
+          curr_position[new_row][5].set_coordinates(new_row, 5);
+          curr_position[new_row][5].set_not_moved(false);
 
           // moving rook
-          curr_position[new_row][3] = curr_position[new_row][0];
-          curr_position[new_row][0] = null;
-          curr_position[new_row][3].set_coordinates(new_row, 3);
-          curr_position[new_row][3].set_not_moved(false);
+          curr_position[new_row][4] = curr_position[new_row][7];
+          curr_position[new_row][7] = null;
+          curr_position[new_row][4].set_coordinates(new_row, 4);
+          curr_position[new_row][4].set_not_moved(false);
         }
 
       } else {
@@ -146,11 +146,11 @@ public class King extends Figure {
     }
 
     if (row == 0 || row == 7) {
-      if (this.can_castle(curr_position, row, 6)) {
-        possible_moves.add(new int[] { row, 6 });
+      if (this.can_castle(curr_position, row, 1)) {
+        possible_moves.add(new int[] { row, 1 });
       }
-      if (this.can_castle(curr_position, row, 2)) {
-        possible_moves.add(new int[] { row, 2 });
+      if (this.can_castle(curr_position, row, 5)) {
+        possible_moves.add(new int[] { row, 5 });
       }
     }
 
